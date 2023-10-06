a9bfc98aaf166cca83b56e4489dbd121148995d0
Replit user <>
2023-10-06 21:24:59 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -91,7 +91,7 @@ int is_valid(Node *n)
         valid_arr(arr2, &result);
     }
 
-    for (int k = 1; k < 4; k++)
+    for (int k = 0; k < 3; k++)
     {
         for (int i = 0; i < 3; i++)
         {
